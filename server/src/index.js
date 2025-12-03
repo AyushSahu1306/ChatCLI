07bd0491bd3f2e8abc91aca8ea59d9e8202287b8
Ayush Sahu <sahuayush0613@gmail.com>
2025-12-03 13:38:21 +0530

    feat: Implement authenticated user profile page

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
+
+app.get("/api/me", async (req, res) => {
+ 	const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+	return res.json(session);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
